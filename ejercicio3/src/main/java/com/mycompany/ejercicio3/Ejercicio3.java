1f8e0fcbc359a1a76877d66e04baa3fcfad4df58
Juan Sandoval <juanchosando@outlook.es>
2024-09-17 21:04:24 -0600

    ejercicio 3

---
+++ ejercicio3/src/main/java/com/mycompany/ejercicio3/Ejercicio3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio3;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 50; i += 5) {
+            System.out.println(i);
+        }
+    }
+}
