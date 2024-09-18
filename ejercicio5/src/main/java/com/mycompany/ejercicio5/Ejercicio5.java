5437879beff052be88eefbc1dac10f20fb432edd
Juan Sandoval <juanchosando@outlook.es>
2024-09-17 21:04:37 -0600

    ejercicio 5

---
+++ ejercicio5/src/main/java/com/mycompany/ejercicio5/Ejercicio5.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio5;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
