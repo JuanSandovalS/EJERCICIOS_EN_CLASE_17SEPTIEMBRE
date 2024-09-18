1cfcb353b918a4f7f973f946c2d3681727632fb3
Juan Sandoval <juanchosando@outlook.es>
2024-09-17 21:04:12 -0600

    ejercicio1

---
+++ ejercicio1/src/main/java/com/mycompany/ejercicio1/Ejercicio1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio1;
+
+/**
+ *
+ * @author JUAN
+ */
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
